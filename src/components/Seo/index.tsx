7650b85782beb7afb8b9e25dac04bde775c2b442
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2025-01-20 13:14:21 +0100

    Add background color to body

--- src/components/Seo/index.tsx
+++ src/components/Seo/index.tsx
@@ -10,7 +10,7 @@ export default function Seo({ title }: { title: String }) {
       </title>
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        content="width=device-width, initial-scale=1, viewport-fit=cover"
       />
     </>
   );
